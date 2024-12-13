515cc2e91879457e9a2a0a7fe5370be79590eced
Anchal Mishra <anchal.mishra@piramal.com>
2024-12-13 17:35:41 +0530

    LSA-1 Started with project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./component/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Router>
     </div>
   );
 }
